dc3b94dd041d56ed3209506585821e3d730d0c14
Jason-Ng00 <e0407783@u.nus.edu>
2021-06-23 11:22:33 +0800

    finish

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "gatsby-example",
+    title: "My First Gatsby Site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+  "gatsby-plugin-gatsby-cloud",
+  "gatsby-plugin-image",
+  "gatsby-plugin-sharp",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
+  ],
 };
